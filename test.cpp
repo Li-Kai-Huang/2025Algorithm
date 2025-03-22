5cf694dd5308720c26c46227ef26cb09c11ed5f0
Li-Kai-Huang <80050626+Li-Kai-Huang@users.noreply.github.com>
2025-03-22 16:09:09 +0800

    新增 C++ 合併排序範例程式及 VSCode 設定檔

--- test.cpp
+++ test.cpp
@@ -1,6 +1,15 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+
 using namespace std;
 
-int main(){
-    cout<<"Hollow" ;
+int main()
+{
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    for (const string& word : msg)
+    {
+        cout << word << " ";
+    }
+    cout << endl;
 }
