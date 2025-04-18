cf908370d971a2b89b2cbde3ad5ae332655b2e98
Li-Kai-Huang <80050626+Li-Kai-Huang@users.noreply.github.com>
2025-04-18 15:45:03 +0800

    新增 1_3_Number_of_Inversions.cpp 檔案，並調整 merge_sort.cpp 中的程式碼格式

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -2,7 +2,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void MERGE(vector<int>& A, int p, int q, int r)
+void MERGE(vector<int> &A, int p, int q, int r)
 {
     int n1 = q - p + 1;
     int n2 = r - q;
@@ -13,7 +13,7 @@ void MERGE(vector<int>& A, int p, int q, int r)
     }
     for (int j = 0; j < n2; j++)
     {
-        R[j] = A[q + j +1 ];
+        R[j] = A[q + j + 1];
     }
     L[n1] = INT_MAX, R[n2] = INT_MAX;
     int i = 0, j = 0;
@@ -26,11 +26,11 @@ void MERGE(vector<int>& A, int p, int q, int r)
     }
 }
 
-void MERGE_SORT(vector<int>& A, int p, int r)
+void MERGE_SORT(vector<int> &A, int p, int r)
 {
     if (p < r)
     {
-        int q = p+(r-p) / 2;
+        int q = p + (r - p) / 2;
         MERGE_SORT(A, p, q);
         MERGE_SORT(A, q + 1, r);
         MERGE(A, p, q, r);
@@ -41,7 +41,7 @@ int main()
     vector<int> array;
     int num;
     string input;
-    getline(cin, input);  // 讀取整行輸入
+    getline(cin, input); // 讀取整行輸入
 
     stringstream ss(input);
     while (ss >> num)
